e0dc16c35df43bc7a4c8fa620b4b3fb35e6741c1
Lyuben Horozov <lhorozov@yahoo.com>
2025-04-16 00:11:49 +0300

    add baseURL to axios settings

--- redux-starter/src/store/task.js
+++ redux-starter/src/store/task.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from 'axios';
+import axios from '../utils/http';
 
 let id = 0;
 
@@ -11,7 +11,7 @@ const initialState = {
 
 export const fetchTasks = createAsyncThunk('fetchTasks', async (a, { rejectWithValue }) => {
     try {
-        const response = await axios.get('http://localhost:5000/api/tasks');
+        const response = await axios.get('/tasks');
         return { tasks: response.data };
     } catch (error) {
         return rejectWithValue({ error: error.message });
