841fde9c61021c60554e037524475625b9813d9d
Lyuben Horozov <lhorozov@yahoo.com>
2025-04-15 14:51:55 +0300

    dispatch events

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,6 +1,9 @@
-console.log("Redux Starter Project!!");
+import store from './store';
 
-function greeting(abc) {
-    return 'Good morning';
-}
+store.dispatch({ type: 'ADD_TASK', payload: { task: 'Task 1' } });
 
+console.log(store.getState());
+
+store.dispatch({ type: 'REMOVE_TASK', payload: { id: 1 } });
+
+console.log(store.getState());
