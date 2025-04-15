3ef4eb294ffa7aedfdcf25731210a621b2f02eb6
Lyuben Horozov <lhorozov@yahoo.com>
2025-04-15 19:30:50 +0300

    add error middleware

--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -3,13 +3,14 @@ import logger from 'redux-logger';
 
 import employeeReducer from './employees';
 import taskReducer from './task';
+import error from '../middleware/error';
 
 const store = configureStore({
     reducer: {
         employee: employeeReducer,
         task: taskReducer,
     },
-    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger],
+    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger, error],
 });
 
 export default store;
