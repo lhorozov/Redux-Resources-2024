3c0a07ef3da9f25d191af1dcf65ec0348492b404
Lyuben Horozov <lhorozov@yahoo.com>
2025-04-15 15:51:55 +0300

    add redux-toolkit

--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -1,7 +1,6 @@
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import reducer from './task';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const store = configureStore({ reducer });
 
 export default store;
