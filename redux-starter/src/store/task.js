e447c39b7e9e98f0e7c020001b2834ed6e4e8057
Lyuben Horozov <lhorozov@yahoo.com>
2025-04-15 18:15:12 +0300

    add createSlice

--- redux-starter/src/store/task.js
+++ redux-starter/src/store/task.js
@@ -1,34 +1,27 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
-
-// Actions
-export const addTask = createAction('ADD_TASK');
-export const removeTask = createAction('REMOVE_TASK');
-export const completeTask = createAction('COMPLETE_TASK');
-
-// for async tasks we just simply return async function instead of object
-export const fetchTodo = () => async (dispatch) => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    const task = await response.json();
-    dispatch(addTask(task.title));
-}
-
-// Reducer
+import { createSlice } from "@reduxjs/toolkit";
 let id = 0;
 
-export default createReducer([], (builder) => { // we are using mutable changes here
-    builder.addCase(addTask, (state, action) => {
-        state.push({
-            id: ++id,
-            task: action.payload.task,
-            completed: false
-        })
-    })
-    builder.addCase(removeTask, (state, action) => {
-        const index = state.findIndex(task => task.id === action.payload.id);
-        state.splice(index, 1);
-    })
-    builder.addCase(completeTask, (state, action) => {
-        const index = state.findIndex(task => task.id === action.payload.id);
-        state[index].completed = true;
-    })
-})
+const taskSlice = createSlice({
+    name: 'tasks',
+    initialState: [],
+    reducers: {
+        addTask: (state, action) => {
+            state.push({
+                id: ++id,
+                task: action.payload.task,
+                completed: false
+            })
+        },
+        removeTask: (state, action) => {
+            const index = state.findIndex(task => task.id === action.payload.id);
+            state.splice(index, 1);
+        },
+        completeTask: (state, action) => {
+            const index = state.findIndex(task => task.id === action.payload.id);
+            state[index].completed = true;
+        }
+    }
+});
+
+export const { addTask, removeTask, completeTask } = taskSlice.actions;
+export default taskSlice.reducer;
