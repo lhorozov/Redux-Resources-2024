a91c96fc7fd8b30a9db5554846192d1d5094eee0
Lyuben Horozov <lhorozov@yahoo.com>
2025-04-15 19:20:50 +0300

    add log middleware

--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import employeeReducer from './employees';
 import taskReducer from './task';
+import log from '../middleware/log'
 
 const store = configureStore({
     reducer: {
         employee: employeeReducer,
         task: taskReducer,
-    } 
- });
+    },
+    middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), log],
+});
 
 export default store;
