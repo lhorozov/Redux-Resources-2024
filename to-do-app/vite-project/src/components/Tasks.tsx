3ab3e043736f099e2d2322dad1280fbd9358e00e
Lyuben Horozov <lhorozov@yahoo.com>
2025-04-20 23:53:25 +0300

    fetching the data (redux way)

--- to-do-app/vite-project/src/components/Tasks.tsx
+++ to-do-app/vite-project/src/components/Tasks.tsx
@@ -1,12 +1,33 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import StoreContext from '../context/storeContext'
+// @ts-ignore
+import { loadTasks } from '../store/task'
 
 const Tasks = () => {
     const store = useContext(StoreContext);
-    console.log(store);
+    // @ts-ignore
+    const [tasks, setTasks] = useState<{ task: string, id: string }[]>([])
+
+    useEffect(() => {
+        console.log(store)
+        // @ts-ignore
+        store.dispatch(loadTasks())
+                // @ts-ignore
+        const unsubscribe = store.subscribe(() => {
+                    // @ts-ignore
+            const storeTasks = store.getState().task.tasks;
+            if (storeTasks !== tasks)
+                setTasks(storeTasks)
+        })
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
     return (
         <div>
-            Tasks
+            {tasks.map(task => <p key={task.id}>{task.id} {task.task}</p>)}
         </div>
     )
 }
