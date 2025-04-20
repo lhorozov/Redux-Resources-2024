2bdfca37520e796ebd7d2ccb2d9d5f03967bbec3
Lyuben Horozov <lhorozov@yahoo.com>
2025-04-20 23:22:30 +0300

    change existing App

--- to-do-app/vite-project/src/App.tsx
+++ to-do-app/vite-project/src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Tasks from './components/Tasks'
+import StoreContext from './context/storeContext'
+// @ts-ignore
+import store from './store/configureStore'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <StoreContext.Provider value={store}>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Tasks />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </StoreContext.Provider>
   )
 }
 
